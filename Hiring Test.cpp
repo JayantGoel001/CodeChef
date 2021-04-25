af29b4a2c0b89e0bf4e3d55f703fe96c04f7ddc8
JayantGoel001 <jgoel92@gmail.com>
2021-04-25 16:14:30 +0530

    Initial Commit

--- Hiring Test.cpp
+++ Hiring Test.cpp
@@ -1,4 +1,30 @@
-//
-// Created by Hannah on 4/25/2021.
-//
-
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int n,m;
+        int x,y;
+        cin>>n>>m;
+        cin>>x>>y;
+        while (n--){
+            string str;
+            cin>>str;
+            map<char,int> mp;
+            mp['U'] = 0;
+            mp['P'] = 0;
+            mp['F'] = 0;
+            for (int i = 0; i < m; ++i) {
+                mp[str[i]]++;
+            }
+            if (mp['F']>=x || (mp['F']>=x-1 && mp['P']>=y)){
+                cout<<"1";
+            } else{
+                cout<<"0";
+            }
+        }
+        cout<<"\n";
+    }
+}
