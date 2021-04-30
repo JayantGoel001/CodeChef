9bbca6d5e93fe57e23d90c7ea10ae6dc0e7b3727
JayantGoel001 <jgoel92@gmail.com>
2021-04-30 20:22:44 +0530

    Initial Commit

---
+++ Array Rotation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#define ll long long int
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    ll xq;
+    ll mod = (ll)pow(10,9)+7;
+    ll sum = (ll)0;
+    for (int i = 0; i < n; ++i) {
+        cin>>xq;
+        sum=(sum+xq)%mod;
+    }
+    int q;
+    cin>>q;
+    int x;
+    for (int i = 0; i < q; ++i) {
+        cin>>x;
+        sum = (sum*2)%mod;
+        cout<<sum<<"\n";
+    }
+}
