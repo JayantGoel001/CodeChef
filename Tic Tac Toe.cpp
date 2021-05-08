32bf2a35d5458111138d40df4ab46611efe74bfb
JayantGoel001 <jgoel92@gmail.com>
2021-05-08 12:21:51 +0530

    Initial Commit

--- Tic Tac Toe.cpp
+++ Tic Tac Toe.cpp
@@ -2,34 +2,37 @@
 #include <vector>
 using namespace std;
 
-bool row(char **ar,char move){
+int row(char **ar,char move){
+    int count = 0;
     for (int i = 0; i < 3; ++i) {
         if (ar[i][0]==ar[i][1] && ar[i][2]==ar[i][1] && ar[i][0]==move){
-            return true;
+            count++;
         }
     }
-    return false;
+    return count;
 }
-bool column(char **ar,char move){
+int column(char **ar,char move){
+    int count=0;
     for (int i = 0; i < 3; ++i) {
         if (ar[0][i]==ar[1][i] && ar[2][i]==ar[1][i] && ar[0][i]==move){
-            return true;
+            count++;
         }
     }
-    return false;
+    return count;
 }
-bool diagonal(char **ar,char move){
+int diagonal(char **ar,char move){
+    int count=0;
     if (ar[0][0]==ar[1][1] && ar[1][1]==ar[2][2] && ar[0][0]==move){
-        return true;
+        count++;
     }
     if (ar[0][2]==ar[1][1] && ar[1][1]==ar[2][0] && ar[0][2]==move){
-        return true;
+        count++;
     }
-    return false;
+    return count;
 }
 
-bool gameOver(char **ar,char move){
-    return row(ar,move) || column(ar,move) || diagonal(ar,move);
+int gameOver(char **ar,char move){
+    return row(ar,move) + column(ar,move) + diagonal(ar,move);
 }
 int main(){
     int t;
@@ -38,7 +41,6 @@ int main(){
         int countX = 0;
         int countO = 0;
         int count_ = 0;
-        vector<pair<int,int>> v;
         char **ar = new char *[3];
         for (int i = 0; i < 3; ++i) {
             ar[i] = new char [3];
@@ -52,38 +54,17 @@ int main(){
                     countO++;
                 } else{
                     count_++;
-                    v.emplace_back(i,j);
                 }
             }
         }
-        bool isGameOverX = gameOver(ar,'X');
-        bool isGameOverO = gameOver(ar,'O');
-
-        if ((isGameOverO && isGameOverX) || countO>countX){
+        int isGameOverX = gameOver(ar,'X');
+        int isGameOverO = gameOver(ar,'O');
+        if (countX<countO || countX>countO+1 ||(isGameOverX && isGameOverO) ||(isGameOverX && countX==countO) || (isGameOverO && countX>countO)){
             cout<<"3\n";
-        }else if (isGameOverX || isGameOverO || count_==0){
+        } else if (isGameOverO || isGameOverX || count_==0){
             cout<<"1\n";
-        }else {
-            char move;
-            if (countX == countO) {
-                move = 'X';
-            } else {
-                move = 'O';
-            }
-            bool isGameOver = false;
-            for (auto & i : v) {
-                ar[i.first][i.second] = move;
-                if (gameOver(ar,move)){
-                    isGameOver = true;
-                    break;
-                }
-                ar[i.first][i.second] = '_';
-            }
-            if (isGameOver){
-                cout<<"1\n";
-            } else{
-                cout<<"2\n";
-            }
+        } else{
+            cout<<"2\n";
         }
     }
 }
