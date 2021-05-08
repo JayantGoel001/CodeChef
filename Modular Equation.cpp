eeaed26a2b8c485c26b0f392b414ad01b1acafe1
JayantGoel001 <jgoel92@gmail.com>
2021-05-08 15:12:16 +0530

    Initial Commit

--- Modular Equation.cpp
+++ Modular Equation.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <valarray>
+
 #define ll long long int
 using namespace std;
 int main(){
@@ -11,10 +13,22 @@ int main(){
         ll n,m;
         cin>>n>>m;
         ll count = 0;
-        for (int i = 1; i <= n; ++i) {
-            ll j = m/i;
-            if (m%i==0 && m%j==0){
-                count++;
+        for (int i = 1; i <=n ; ++i) {
+            for (int j = i+1; j <=n ; ++j) {
+                if ((m%i)  == (m%j)%i){
+                    count++;
+                    cout<<i<<" "<<j<<"\n";
+                }
+            }
+        }
+        cout<<count<<"\n";
+        count=n-1;
+        for (int i = 2; i <= ceil(sqrt(n)); ++i) {
+            for (int j = ceil(sqrt(n)); j <=n ; ++j) {
+                if (i!=j && (m%i)  == (m%j)%i){
+                    count++;
+                    cout<<i<<" "<<j<<"\n";
+                }
             }
         }
         cout<<count<<"\n";
