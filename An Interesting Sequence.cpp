fe57f3f73bec5bbc8e3633cd6eedf96f0fa6f2d6
JayantGoel001 <jgoel92@gmail.com>
2021-05-10 01:46:25 +0530

    Initial Commit

--- An Interesting Sequence.cpp
+++ An Interesting Sequence.cpp
@@ -1,85 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <valarray>
-
+#include <cmath>
+#include <algorithm>
 #define ll long long int
 using namespace std;
-ll GCD(ll a,ll b){
-    if (b==0){
-        return a;
-    }
-    return GCD(b,a%b);
-}
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int t;
     cin>>t;
-//    int TOT = 4*(int) pow(10,6)+2;
-    int TOT = (int) pow(10,6)+1;
-    bool isPrime[TOT];
-    for (int i = 1; i <=TOT ; ++i) {
-        isPrime[i] = true;
-    }
-    for (int i = 2; i*i <=TOT ; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= TOT; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
+    vector<int> vs,vp;
     while (t--){
         int k;
         cin>>k;
-        ll sum = 0;
-        vector<ll> v;
-        for (int i = 1; i <=2*k+1 ; ++i) {
-            v.push_back(k + i*i);
-        }
-        for (long long i : v) {
-            cout<<i<<" ";
-        }
-        cout<<"\n";
-        for (int i = 0; i <v.size()-1; ++i) {
-            sum+=GCD(v[i],v[i+1]);
-            cout<<GCD(v[i],v[i+1])<<" ";
-        }
-        cout<<"\n";
-        cout<<sum<<"\n";
-        ll lastNumber = 4*k+1;
-        if (isPrime[lastNumber]){
-            cout<<lastNumber + (2*k-1)<<"\n";
-        } else {
-            sum = 0;
-            for (int i = 1; i <= 2 * k; ++i) {
-                if (!isPrime[k + i * i] || !isPrime[2*i+1]) {
-                    sum += GCD(k + i * i, 2 * i + 1);
-                } else {
-                    sum += 1;
+        ll lastNum = 4*k+1;
+        ll n = 2*k;
+        vector<ll> v,factors,seq;
+        for (int i = 2; i <=sqrt(lastNum) ; ++i) {
+            if (lastNum%i==0){
+                factors.push_back(i);
+                if (i*i!=lastNum){
+                    factors.push_back(lastNum/i);
                 }
             }
-            cout<<"\n";
-            cout << sum << "\n";
         }
-//            vector<ll> factors;
-//            for (int i = 2; i < lastNumber ; ++i) {
-//                if (lastNumber%i==0){
-//                    factors.push_back(i);
-//                }
-//            }
-//            sum = lastNumber;
-//            ll num = 0;
-//            for (int i = factors.size()-1; i >=0; i--) {
-//                ll x = (2*k-num)/factors[i];
-//                sum += factors[i] * x;
-//                num+=x;
-//                cout<<x<<" "<<factors[i]<<"\n";
-//            }
-//            cout<<"\n";
-//            sum+=(2*k-num-1);
-//            cout<<sum<<"\n";
-//        }
-//        for (int i = 1; i <=2*k ; ++i) {
-//            ll diff = 2*i+1;
-//
-//        }
+        sort(factors.begin(), factors.end());
+        for (int i = 0; i < factors.size(); ++i) {
+            seq.push_back(n/factors[i]);
+        }
+        for(int i=factors.size()-1;i>=0;i--){
+            for (int j = i-1; j >=0; --j) {
+                if (factors[i]%factors[j]==0){
+                    factors[j]-=factors[i];
+                }
+            }
+        }
+        ll sum = 0;
+        for (int i = 0; i < seq.size(); ++i) {
+            sum+=seq[i];
+        }
+        ll output = lastNum + (n-sum-1);
+        for (int i = 0; i < factors.size(); ++i) {
+            output+=factors[i]*seq[i];
+        }
+        cout<<output<<"\n";
     }
 }
