31e8fe7bd9f3ce8b227da7d5b183892b17242d2c
JayantGoel001 <jgoel92@gmail.com>
2021-05-16 14:12:19 +0530

    Initial Commit

--- King Killing.cpp
+++ King Killing.cpp
@@ -1,47 +1,40 @@
 #include<iostream>
 #include <vector>
 #include <algorithm>
-
 #define ll long long int
 using namespace std;
 class Tree{
 public:
     ll data,height;
     vector<Tree*> children;
-    Tree(){
-        this->data = 0;
-    }
     Tree(ll data){
         this->data = data;
         this->height = 0;
     }
 };
-bool cmp(const pair<ll,ll> &a,const pair<ll,ll> &b){
-    return a.second<b.second;
-}
-void findHeight(Tree *tree,ll height){
-    tree->height = height;
-    for (int i = 0; i < tree->children.size(); ++i) {
-        findHeight(tree->children[i],height+1);
-    }
-}
-void findLeaves(Tree *tree,vector<pair<ll,ll>> &leaves){
-    for (int i = 0; i < tree->children.size(); ++i) {
-        findLeaves(tree->children[i],leaves);
-    }
-    if (tree->children.empty()){
-        leaves.emplace_back(tree->data+1,tree->height);
-    }
-}
-void adjacentListToTree(Tree *tree,vector<vector<ll>> &adj,bool *visited){
+void adjacentListToTree(Tree *tree,vector<vector<ll>> &adj,vector<bool> &visited){
     visited[tree->data] = true;
-    for (int i = 0; i < adj[tree->data].size(); ++i) {
-        if (!visited[adj[tree->data][i]]){
-            tree->children.push_back(new Tree(adj[tree->data][i]));
+    for (auto it:adj[tree->data]) {
+        if (!visited[it]){
+            tree->children.push_back(new Tree(it));
         }
     }
     for(auto it:tree->children){
         adjacentListToTree(it,adj,visited);
+    }
+}
+void findHeight(Tree *tree,ll height){
+    tree->height = height;
+    for (auto it:tree->children) {
+        findHeight(it,height+1);
+    }
+}
+void findLeaves(Tree *tree,vector<pair<ll,ll>> &leaves){
+    for (auto it:tree->children) {
+        findLeaves(it,leaves);
+    }
+    if (tree->children.empty()){
+        leaves.push_back(make_pair(tree->height,tree->data+1));
     }
 }
 int main(){
@@ -54,59 +47,60 @@ int main(){
         int n;
         cin>>n;
         vector<vector<ll>> adj(n);
-        bool *visited = new bool[n];
+        vector<bool> visited(n);
         for (int j = 0; j < n; ++j) {
             visited[j] = false;
         }
         Tree *tree = new Tree(0);
         vector<pair<ll,ll>> output;
-        vector<pair<ll,ll>> leaves;
         for (int i = 0; i < n - 1; ++i) {
             ll u,v;
             cin>>u>>v;
-            adj[u-1].push_back(v-1);
-            adj[v-1].push_back(u-1);
+            u-=1;
+            v-=1;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         adjacentListToTree(tree,adj,visited);
         findHeight(tree,0);
         for (int i = 0; i < tree->children.size(); ++i) {
+            vector<pair<ll,ll>> leaves;
             findLeaves(tree->children[i],leaves);
-            sort(leaves.begin(),leaves.end(),cmp);
+            sort(leaves.begin(),leaves.end());
             pair<ll,ll> pr = leaves[0];
             vector<pair<ll,ll>> vp;
             vp.push_back(pr);
             bool found= false;
-            ll value = pr.second;
-            int j = 1;
+            ll value = pr.first;
+            ll j = 1;
             while (j<leaves.size()){
-                while (j<leaves.size() && leaves[j].second==value){
+                while (j<leaves.size() && leaves[j].first==value){
                     if (found){
-                        vp.emplace_back(leaves[j].first,pr.second+1);
+                        vp.push_back(make_pair(pr.first+1,leaves[j].second));
                     } else{
-                        vp.emplace_back(leaves[j]);
+                        vp.push_back(make_pair(leaves[j].first,leaves[j].second));
                     }
                     j+=1;
                 }
                 if (j<leaves.size()){
                     vp.clear();
                     found = true;
-                    pr.first = leaves[j].first;
-                    vp.emplace_back(leaves[j].first,pr.second+1);
-                    value = leaves[j].second;
+                    pr.second = leaves[j].second;
+                    vp.push_back(make_pair(pr.first+1,leaves[j].second));
+                    value = leaves[j].first;
                     j+=1;
                 }
             }
-            leaves.clear();
             for (int k = 0; k < vp.size(); ++k) {
-                output.emplace_back(vp[k]);
+                output.push_back(make_pair(vp[k].first,vp[k].second));
             }
         }
-        sort(output.begin(), output.end(),cmp);
-        ll numberOfDays = output[0].second;
+        sort(output.begin(), output.end());
+        ll numberOfDays = output[0].first;
         vector<ll> result;
         for(auto it:output){
-            if (it.second==numberOfDays){
-                result.push_back(it.first);
+            if (it.first==numberOfDays){
+                result.push_back(it.second);
             } else{
                 break;
             }
