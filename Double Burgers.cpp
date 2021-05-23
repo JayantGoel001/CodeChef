1b3a7c44f14d07bc1f2524ec4c844c087b0b1706
JayantGoel001 <jgoel92@gmail.com>
2021-05-23 23:07:56 +0530

    Initial Commit

---
+++ Double Burgers.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <valarray>
+
+#define int long long int
+using namespace std;
+int power(int x,int y){
+    if(y==0){
+        return 1;
+    }
+    if (y%2==0){
+        return power(x*x,y/2);
+    } else{
+        return x * power(x*x,(y-1)/2);
+    }
+}
+int calculateGP(int n){
+    return (power(2,n)-1);
+}
+int calculateEaten(int x,int y){
+    return floor(log((y+x)/x)/log(2.0));
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int x,y;
+        cin>>x>>y;
+        if (y%x!=0){
+            cout<<"-1\n";
+        } else{
+            int count = 0;
+            while (y>0){
+                int time = calculateEaten(x,y);
+                count+=time;
+                y = y - x * calculateGP(time);
+                if (y!=0){
+                    count++;
+                }
+            }
+            cout<<count<<"\n";
+        }
+    }
+}
