f090094ca7dcee7782c3cf01b3d08fa4928e0bea
JayantGoel001 <jgoel92@gmail.com>
2021-05-25 13:01:02 +0530

    Initial Commit

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <valarray>
+
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        cout<<floor(sqrt(n))<<"\n";
+    }
+}
