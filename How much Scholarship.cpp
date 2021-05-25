994cb6dd1d79ca21c4e1f81b94d5cccbbecf1c78
JayantGoel001 <jgoel92@gmail.com>
2021-05-25 06:39:13 +0530

    Initial Commit

---
+++ How much Scholarship.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define int long long int
+using namespace std;
+int32_t main(){
+    int r;
+    cin>>r;
+    if (r<=50){
+        cout<<"100\n";
+    } else if (r<=100){
+        cout<<"50\n";
+    } else{
+        cout<<"0\n";
+    }
+}
