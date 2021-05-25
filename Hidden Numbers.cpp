dd26596e7ce31fa15324174bd79dab10af9c5bc6
JayantGoel001 <jgoel92@gmail.com>
2021-05-25 05:30:27 +0530

    Initial Commit

---
+++ Hidden Numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        cout<<1<<" "<<n<<"\n";
+    }
+}
