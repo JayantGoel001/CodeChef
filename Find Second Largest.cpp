45733d7917278ad6af871d43fc50c5e585850672
JayantGoel001 <jgoel92@gmail.com>
2021-05-27 00:15:11 +0530

    Initial Commit

---
+++ Find Second Largest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#define int long long int
+using namespace std;
+int32_t main(){
+    int ar[3];
+    for (int i = 0; i < 3; ++i) {
+        cin>>ar[i];
+    }
+    sort(ar,ar+3);
+    cout<<ar[1]<<"\n";
+}
