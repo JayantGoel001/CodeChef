95d800f577d3d4fbc152df0719857c9eb9e0bafe
JayantGoel001 <jgoel92@gmail.com>
2021-05-27 00:39:23 +0530

    Initial Commit

--- Triangle With Angle.cpp
+++ Triangle With Angle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
-    if (a+b+c==180){
+    if (a+b+c==180 && a!=0 && b!=0 && c!=0){
         cout<<"YES\n";
     } else{
         cout<<"NO\n";
