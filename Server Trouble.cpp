a9f8edd74abb4b4d36f1db23f19a2b1dd4254beb
JayantGoel001 <jgoel92@gmail.com>
2021-05-26 15:49:58 +0530

    Initial Commit

---
+++ Server Trouble.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <valarray>
+
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int n,k;
+        cin>>n>>k;
+        if (n%k==0){
+            cout << n/k << " " << k << "\n";
+        } else {
+            cout << n/k+1 << " " << n % k << "\n";
+        }
+    }
+}
