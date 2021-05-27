c93cefa7470251e6a406fb5945ff4c4cd3ad3234
JayantGoel001 <jgoel92@gmail.com>
2021-05-27 15:04:26 +0530

    Initial Commit

---
+++ Coin Flip.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int g;
+        cin>>g;
+        int I,N,Q;
+        cin>>I>>N>>Q;
+
+    }
+}
