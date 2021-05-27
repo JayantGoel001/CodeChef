10a40ed6f97b90a214aae197af76e9e6940c01f1
JayantGoel001 <jgoel92@gmail.com>
2021-05-27 16:18:51 +0530

    Initial Commit

--- Coin Flip.cpp
+++ Coin Flip.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int t;
     cin>>t;
     while (t--){
         int g;
         cin>>g;
-        int I,N,Q;
-        cin>>I>>N>>Q;
-
+        for (int _ = 0; _ < g; ++_) {
+            int I, N, Q;
+            cin >> I >> N >> Q;
+            int count;
+            if (I==1){
+                if (Q==1){
+                    count = N/2;
+                } else{
+                    count = N - N/2;
+                }
+            } else{
+                if (Q==2){
+                    count = N/2;
+                } else{
+                    count = N - N/2;
+                }
+            }
+            cout << count << "\n";
+        }
     }
 }
