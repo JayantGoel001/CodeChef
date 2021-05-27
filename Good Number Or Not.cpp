a863a409b7a9ee1770d16c04fc27ac9fc2cb0043
JayantGoel001 <jgoel92@gmail.com>
2021-05-27 15:28:42 +0530

    Initial Commit

--- Good Number Or Not.cpp
+++ Good Number Or Not.cpp
@@ -4,13 +4,16 @@
 #define int long long int
 using namespace std;
 int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n;
     cin>>n;
     int sum = 0;
-    for (int i = 1; i <=ceil(sqrt(n)) ; ++i) {
+    for (int i = 1; i <=sqrt(n) ; ++i) {
         if (n%i==0){
             sum+=i;
-            if (n/i!=i){
+            if (i!=1 && n/i!=i){
                 sum+=n/i;
             }
         }
