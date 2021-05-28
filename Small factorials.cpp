e78ad14836b2725051e066c97f4222423bcb73c9
JayantGoel001 <jgoel92@gmail.com>
2021-05-29 01:22:20 +0530

    Initial Commit

--- Small factorials.cpp
+++ Small factorials.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+#define int unsigned long long int
 using namespace std;
-int main(){
+int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
