84777e3a3394e3477834cda9076888a47fdf78d2
JayantGoel001 <jgoel92@gmail.com>
2021-05-29 15:07:42 +0530

    Initial Commit

---
+++ Helping Chef.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        if (n<10){
+            cout<<"Thanks for helping Chef!\n";
+        } else{
+            cout<<"-1\n";
+        }
+    }
+}
