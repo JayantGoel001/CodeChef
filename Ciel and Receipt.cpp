86c04af3d3e7c2fd5577fbe3a8ee268870df200b
JayantGoel001 <jgoel92@gmail.com>
2021-05-31 18:49:25 +0530

    Initial Commit

---
+++ Ciel and Receipt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <valarray>
+
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int x;
+        cin>>x;
+        int count = 0;
+        while (x!=0){
+            int w = log(x)/log(2);
+            x-= pow(2,w);
+            count++;
+        }
+        cout<<count<<"\n";
+    }
+}
