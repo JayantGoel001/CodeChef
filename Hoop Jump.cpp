8a0cb74b3f1c63ac1e54bac360fdc458c99230ae
JayantGoel001 <jgoel92@gmail.com>
2021-06-01 20:05:17 +0530

    Initial Commit

---
+++ Hoop Jump.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        cout<<(1+n)/2<<"\n";
+    }
+}
