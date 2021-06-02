5ab39c8be887c97e41503da021d10762f7f9c1b3
JayantGoel001 <jgoel92@gmail.com>
2021-06-02 18:38:00 +0530

    Initial Commit

--- Version Control System.cpp
+++ Version Control System.cpp
@@ -8,6 +8,34 @@ int32_t main(){
     int t=1;
     cin>>t;
     while(t--){
-
+        int m,k,n;
+        cin>>n>>m>>k;
+        int a[m],b[k];
+        for (int i = 0; i < m; ++i) {
+            cin>>a[i];
+        }
+        for (int i = 0; i < k; ++i) {
+            cin>>b[i];
+        }
+        int ar[n+1];
+        for (int i = 1; i <= n; ++i) {
+            ar[i] = 0;
+        }
+        for (int i = 0; i < m; ++i) {
+            ar[a[i]]--;
+        }
+        for (int i = 0; i < k; ++i) {
+            ar[b[i]]--;
+        }
+        int countTI = 0;
+        int countNTI = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (ar[i]==0){
+                countNTI++;
+            } else if (ar[i]==-2){
+                countTI++;
+            }
+        }
+        cout<<countTI<<" "<<countNTI<<"\n";
     }
 }
