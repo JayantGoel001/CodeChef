ff208d9394c8c456e2183759ae91f7ccc0a21c81
JayantGoel001 <jgoel92@gmail.com>
2021-06-02 18:46:46 +0530

    Initial Commit

---
+++ Digit Longest Increasing Subsequences 2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin>>x;
+            cout<<x;
+        }
+        cout<<"\n";
+    }
+}
