eefb091eb99ce5e326561b0e6d0eb40d5407004f
JayantGoel001 <jgoel92@gmail.com>
2021-06-02 22:38:51 +0530

    Initial Commit

---
+++ Mutated Minions.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int count = 0;
+        int x;
+        for (int i = 0; i < n; ++i) {
+            cin>>x;
+            if ((x+10)%7==0){
+                count++;
+            }
+        }
+        cout<<count<<"\n";
+    }
+}
