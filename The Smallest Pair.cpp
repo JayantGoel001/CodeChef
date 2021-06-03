46eb767d29d46c145e07eeb43eb7b7028a785bb2
JayantGoel001 <jgoel92@gmail.com>
2021-06-03 22:08:49 +0530

    Initial Commit

---
+++ The Smallest Pair.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+        }
+        sort(ar,ar+n);
+        cout<<ar[0]+ar[1]<<"\n";
+    }
+}
