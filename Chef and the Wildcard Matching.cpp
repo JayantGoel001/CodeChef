56c982cb5e3445b31780248382697e03730dea24
Jayant Goel <jgoel92@gmail.com>
2021-06-28 01:56:25 +0530

    Initial Commit

---
+++ Chef and the Wildcard Matching.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+bool compareString(string x,string y){
+    for (int i = 0; i < x.size(); ++i) {
+        if (!(x[i] == y[i] || x[i] == '?' || y[i] == '?')){
+            return false;
+        }
+    }
+    return true;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        string x,y;
+        cin>>x;
+        cin>>y;
+        if (compareString(x,y)){
+            cout<<"Yes\n";
+        } else{
+            cout<<"No\n";
+        }
+    }
+}
