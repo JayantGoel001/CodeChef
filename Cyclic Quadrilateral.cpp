b48e668cf3d78fb4a04d8e37f04833e9c5e3f4bc
Jayant Goel <jgoel92@gmail.com>
2021-06-27 16:21:23 +0530

    Initial Commit

---
+++ Cyclic Quadrilateral.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int a,b,c,d;
+        cin >> a>>b>>c>>d;
+        if (a+c==180 && b+d == 180){
+            cout<<"YES\n";
+        } else{
+            cout<<"NO\n";
+        }
+    }
+}
