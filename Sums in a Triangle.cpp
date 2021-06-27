02b4fc69304e98af99022537704ab59291adfe3c
Jayant Goel <jgoel92@gmail.com>
2021-06-28 01:23:52 +0530

    Initial Commit

--- Sums in a Triangle.cpp
+++ Sums in a Triangle.cpp
@@ -10,6 +10,22 @@ int32_t main(){
     while(t--){
         int n;
         cin>>n;
-
+        int ar[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ar[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                cin>>ar[i][j];
+            }
+        }
+        for (int i = n-2; i>=0 ; --i) {
+            for (int j = 0; j <= i; ++j) {
+                ar[i][j] += max(ar[i+1][j],ar[i+1][j+1]);
+            }
+        }
+        cout<<ar[0][0]<<"\n";
     }
 }
