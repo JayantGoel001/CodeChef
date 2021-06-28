b4619380cfcc8870c27fcdef88e081301f4a9156
Jayant Goel <jgoel92@gmail.com>
2021-06-28 14:06:26 +0530

    Initial Commit

---
+++ Chefland Visa.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int x1,x2,y1,y2,z1,z2;
+        cin >> x1>>x2>>y1>>y2>>z1>>z2;
+        if (x1<=x2 && y1<=y2 && z2<=z1){
+            cout<<"YES\n";
+        } else{
+            cout<<"NO\n";
+        }
+    }
+}
