fb8f9fd2450badfc1d9e59eb1901a6ce3a1f893a
JayantGoel001 <jgoel92@gmail.com>
2021-07-02 22:54:43 +0530

    Initial Commit

---
+++ The Gray-Similar Code.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+
+int power(int x, int y, int MOD = INF) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2) % MOD) % MOD;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i];
+        }
+        if (n>=130){
+            cout<<"YES\n";
+        } else{
+            bool found = false;
+            for (int i = 0; i < n; ++i) {
+                for (int j = i+1; j <n ; ++j) {
+                    for (int k = j+1; k <n ; ++k) {
+                        int x = ar[i]^ar[j]^ar[k];
+                        int start = k+1;
+                        int last = n-1;
+                        int pos = -1;
+                        while (start<=last){
+                            int mid = (start+last)/2;
+                            if (ar[mid]==x){
+                                pos = mid;
+                                break;
+                            } else if (ar[mid]<x){
+                                start = mid + 1;
+                            } else{
+                                last = mid - 1;
+                            }
+                        }
+                        if (pos!=-1){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found){
+                        break;
+                    }
+                }
+                if (found){
+                    break;
+                }
+            }
+            if (!found){
+                cout<<"NO\n";
+            } else{
+                cout<<"YES\n";
+            }
+        }
+        cout << "\n";
+    }
+}
