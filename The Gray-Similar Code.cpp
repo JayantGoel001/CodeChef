718a43ee431b08706936ed0d4483097e86c8a4fd
JayantGoel001 <jgoel92@gmail.com>
2021-07-02 23:33:39 +0530

    Initial Commit

--- The Gray-Similar Code.cpp
+++ The Gray-Similar Code.cpp
@@ -42,34 +42,19 @@ int32_t main() {
         if (n>=130){
             cout<<"Yes";
         } else{
-            bool found = false;
             for (int i = 0; i < n; ++i) {
                 for (int j = i+1; j <n ; ++j) {
                     for (int k = j+1; k <n ; ++k) {
                         for (int l = k+1; l < n; ++l) {
-                            if (ar[i]^ar[j]^ar[k]^ar[l]==0){
-                                found = true;
-                                break;
+                            if ((ar[i]^ar[j]^ar[k]^ar[l])==0){
+                                cout<<"Yes\n";
+                                return 0;
                             }
                         }
-                        if (found){
-                            break;
-                        }
-                    }
-                    if (found){
-                        break;
                     }
                 }
-                if (found){
-                    break;
-                }
             }
-            if (!found){
-                cout<<"No";
-            } else{
-                cout<<"Yes";
-            }
+            cout<<"No\n";
         }
-        cout << "\n";
     }
 }
